7263ff2c462c1cf5b1be85f5c298862bc84b03b3
jinjaehyun <jinjapa2002@naver.com>
2022-11-18 16:58:40 +0900

    second commit

--- gongmo/board/models.py
+++ gongmo/board/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Board(models.Model):
-    title = models.CharField(max_length=20,verbose_name= '글 제목')
-    content = models.TextField(verbose_name='글 내용')
+    title = models.CharField(max_length=20,verbose_name= '공모전 이름& 구인인원')
+    content = models.TextField(verbose_name='카카오톡 오픈 채팅방 url')
     user=models.ForeignKey('users.Member',on_delete=models.CASCADE,verbose_name='글 작성자')
     createdDate=models.DateTimeField(auto_now_add=True)
     updatedDate=models.DateTimeField(auto_now=True)
